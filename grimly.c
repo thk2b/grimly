5a72afbab9a5a93b6c7c6e5f589f5fe05aa18281
thk2b <theokobb@gmail.com>
2018-10-23 16:45:41 -0700

    architect

--- grimly.c
+++ grimly.c
@@ -1,18 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   grimly.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/22 10:57:25 by tkobb             #+#    #+#             */
-/*   Updated: 2018/10/23 16:27:48 by tkobb            ###   ########.fr       */
+/*   Created: 2018/10/23 16:26:57 by tkobb             #+#    #+#             */
+/*   Updated: 2018/10/23 16:44:05 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "grimly.h"
 
-int	main(int ac, char **av)
+int	grimly(int fd)
 {
+	t_point		exit;
+	t_point		entrance;
+	t_tile_map	tile_map;
+	t_value_map	dst_map;
+	t_sym		sym;
 
+	if ((entrance = read_tile_map(fd, &tile_map, &entrance, &sym)).x < 0)
+		return (1);
+	if ((exit = bfs(&tile_map, &dst_map, &entrance, &sym)).x < 0)
+		return (1);
+	return (render_path(&tile_map, &dst_map, &dst_map, &sym));
 }
--- grimly.h
+++ grimly.h
@@ -6,11 +6,38 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/22 10:57:08 by tkobb             #+#    #+#             */
-/*   Updated: 2018/10/22 10:57:23 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/10/23 16:43:40 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GRIMLY_H
 # define GRIMLY_H
+
+typedef struct	s_point
+{
+	int	x;
+	int	y;
+}				t_point;
+
+typedef struct	s_tile_map
+{
+	t_point	size;
+	char	**tile;
+}				t_tile_map;
+
+typedef	struct	s_value_map
+{
+	t_point	size;
+	int		**value;
+}				t_value_map;
+
+typedef char	t_sym[5];
+
+int				grimly(int fd);
+t_point			read_tile_map(int fd, t_tile_map* tile_map, t_point *entrance, t_sym *sym);
+t_point			bfs(t_tile_map *tile_map, t_value_map *dst_map,
+	t_point *entrance, t_sym *sym);
+int				render_path(t_tile_map *tile_map, t_value_map *val_map,
+	t_point *exit, t_sym *sym);
 
 #endif
